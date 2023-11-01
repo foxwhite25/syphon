b0c3795a4166bade7fb4a7230de58a1b1c13b5fe
Fox_white <vct.xie@gmail.com>
2023-11-01 17:03:22 +0800

    opt: remove default template

--- src/lib.rs
+++ src/lib.rs
@@ -18,18 +18,3 @@ pub(crate) mod macros;
 
 #[cfg(feature = "extractor")]
 pub mod extractor;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
